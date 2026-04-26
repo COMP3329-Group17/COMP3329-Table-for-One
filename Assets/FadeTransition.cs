bc96e77f755adbec8c8e4df9d3d1c05724a068ff
Jennifer Fu <jenniferfu03@gmail.com>
2026-04-26 16:23:26 +0800

    fade to black transitions

---
+++ Assets/FadeTransition.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public static SceneTransitionManager Instance;
+
+    [SerializeField] private Image fadeOverlay;
+    [SerializeField] private float fadeInDuration = 2f;
+    [SerializeField] private float fadeOutDuration = 1f;
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeToScene(string sceneName)
+    {
+        StartCoroutine(FadeOutAndLoad(sceneName));
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float timer = 0f;
+        Color color = fadeOverlay.color;
+
+        while (timer < fadeInDuration)
+        {
+            timer += Time.deltaTime;
+            color.a = Mathf.Lerp(1f, 0f, timer / fadeInDuration);
+            fadeOverlay.color = color;
+            yield return null;
+        }
+
+        color.a = 0f;
+        fadeOverlay.color = color;
+        fadeOverlay.raycastTarget = false;
+    }
+
+    private IEnumerator FadeOutAndLoad(string sceneName)
+    {
+        fadeOverlay.raycastTarget = true;
+
+        float timer = 0f;
+        Color color = fadeOverlay.color;
+
+        while (timer < fadeOutDuration)
+        {
+            timer += Time.deltaTime;
+            color.a = Mathf.Lerp(0f, 1f, timer / fadeOutDuration);
+            fadeOverlay.color = color;
+            yield return null;
+        }
+
+        color.a = 1f;
+        fadeOverlay.color = color;
+
+        yield return null;
+
+        SceneManager.LoadScene(sceneName);
+
+        StartCoroutine(FadeIn());
+    }
+}
